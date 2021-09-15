184e1ac06857f81ec2f861176958216cb6af1508
Colton Kaiser <kaiserc2@seattleu.edu>
2021-09-15 10:00:15 -0600

    feat: :card_file_box: create prisma db and initial migration.

---
+++ prisma/migrations/20210915015346_create_database/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Game" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "Game_pkey" PRIMARY KEY ("id")
+);
