922515a2c0ba9ea5274fc3baa5910a7fee538018
Colton Kaiser <kaiserc2@seattleu.edu>
2021-09-30 16:52:50 -0600

    feat: :label: create User type and add relation to Game.

--- types.ts
+++ types.ts
@@ -1,6 +1,19 @@
-export type Game = {
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  emailVerified: Date;
+  image: String;
+  games: Game[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface Game {
   id: string;
   createdAt: Date;
   title: string;
   description: string;
-};
+  user?: User;
+  userId?: number;
+}
