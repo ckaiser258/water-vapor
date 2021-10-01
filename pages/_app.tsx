c23689ee5f15d21231bc0e1e29567b8704fa0856
Colton Kaiser <kaiserc2@seattleu.edu>
2021-10-01 16:34:07 -0600

    feat: :sparkles: configure apollo provider.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,30 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { Provider } from "next-auth/client";
+import { Provider as NextAuthProvider } from "next-auth/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+
+// Configure Apollo client
+const httpLink = createHttpLink({
+  uri: "/api",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider session={pageProps.session}>
-      <Component {...pageProps} />
-    </Provider>
+    <NextAuthProvider session={pageProps.session}>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </NextAuthProvider>
   );
 }
 export default MyApp;
