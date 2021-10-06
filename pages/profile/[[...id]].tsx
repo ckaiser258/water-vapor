b16ad748763d35d4257242f7a611951720d7da3a
Colton Kaiser <kaiserc2@seattleu.edu>
2021-10-06 14:00:32 -0600

    feat: :sparkles: set up gql getGamesByUser on profile page.

--- pages/profile/[[...id]].tsx
+++ pages/profile/[[...id]].tsx
@@ -1,26 +1,85 @@
 // [[...id]] so that we can add subsequent routes after this and if needed and include /profile.
 //  ex: '/profile/1/games'
 
-import { getSession, useSession } from "next-auth/client";
+import { useEffect } from "react";
+import { gql, useQuery } from "@apollo/client";
+import { NextPage } from "next";
+import { useSession } from "next-auth/client";
 import { useRouter } from "next/router";
-import React from "react";
-import User from "../../components/user";
+import User from "../../components/users/User";
+import { Game } from "../../types";
+import GameForm from "../../components/games/GameForm";
 
-const ProfilePage = () => {
+const GAMES_BY_USER_QUERY = gql`
+  query gamesByUser($userId: ID!) {
+    getGamesByUser(userId: $userId) {
+      id
+      title
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+
+const ProfilePage: NextPage = () => {
   const router = useRouter();
   const [session, loading] = useSession();
-  return !session && !loading ? (
-    router.push("/login")
-  ) : (
-    <User user={session?.user} />
+  const { data, error } = useQuery(GAMES_BY_USER_QUERY, {
+    variables: {
+      userId: session?.userId,
+    },
+    // Prevent a 400 error from Apollo on initial render
+    skip: !session?.userId,
+  });
+
+  useEffect(() => {
+    // If not logged in, redirect to login page
+    if (!session?.userId && !loading) {
+      router.push("/login");
+    }
+  }, [session, router, loading]);
+
+  const { getGamesByUser } = data || {};
+
+  if (error) console.error(error);
+
+  return (
+    <>
+      {loading && <p>Loading...</p>}
+      <User user={session?.user} />
+      <ul>
+        {getGamesByUser?.map((game: Game) => (
+          <li key={game.id}>{game.title}</li>
+        ))}
+      </ul>
+      <GameForm />
+    </>
   );
 };
 
-export const getServerSideProps = async (context) => {
-  const session = await getSession(context);
-  return {
-    props: { session },
-  };
-};
+// **NOTE**: Leaving this here as an example of how to query gql with variables in getServerSideProps:
+
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   const session = await getSession(context);
+//   // If not signed in, redirect to login page
+//   if (!session || !session.user) {
+//     return { redirect: { destination: "/login", permanent: false } };
+//   }
+
+//   const { data, loading, error } = await client.query({
+//     query: GAMES_BY_USER_QUERY,
+//     variables: { userId: session.userId },
+//   });
+//   const currentUserGames = data.getGamesByUser;
+
+//   const props = {
+//     session,
+//     currentUserGames,
+//     loading,
+//     error: error ? error : null,
+//   };
+//   return { props };
+// };
 
 export default ProfilePage;
