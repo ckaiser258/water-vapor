b7baa048707a3f7648b46dbbf2b5f8d4ca088548
Colton Kaiser <kaiserc2@seattleu.edu>
2021-11-19 15:25:17 -0700

    refactor: :fire: remove console log from getGames.

--- db/game/queries/getGames.ts
+++ db/game/queries/getGames.ts
@@ -1,7 +1,6 @@
 import { Game } from ".prisma/client";
 
 const getGames = async (parent, args, context) => {
-  console.log(context.token);
   const allGames: Game[] = await context.prisma.game.findMany();
   return allGames;
 };
